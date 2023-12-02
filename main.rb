3da6cce55ff85dbc90c5eb3a2a2fa1434e481dae
Alexander Klee <alex.klee@outlook.com>
2023-12-02 12:22:37 +0100

    Update main.rb

--- main.rb
+++ main.rb
@@ -2,9 +2,15 @@ require 'cgi'
 require 'json'
 require 'active_support'
 
+# Verifies and decrypts a session cookie using a secret key base.
+# @param [String] cookie - The encrypted session cookie.
+# @param [String] secret_key_base - The secret key for decryption.
+# @return [String] Decrypted cookie content.
 def verify_and_decrypt_session_cookie(cookie, secret_key_base)
+  raise ArgumentError, 'Cookie cannot be blank' if cookie.to_s.strip.empty?
+  raise ArgumentError, 'Secret key base cannot be blank' if secret_key_base.to_s.strip.empty?
 
-cookie = CGI::unescape(cookie)
+  cookie = CGI::unescape(cookie)
   salt         = 'encrypted cookie'
   signed_salt  = 'signed encrypted cookie'
   key_generator = ActiveSupport::KeyGenerator.new(secret_key_base, iterations: 1000)
